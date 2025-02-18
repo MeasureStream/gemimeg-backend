2b7abb98a9e48a2540a2ff969d841f3f4155aeb0
Samuel Eickelberg <samuel.eickelberg@ptb.de>
2025-02-18 15:40:42 +0100

    OP#18070 Forgot to annotate CalibrationCertificate with @Document

--- gemimeg-backend/src/main/java/de/ptb/common/dcc/model/CalibrationCertificate.java
+++ gemimeg-backend/src/main/java/de/ptb/common/dcc/model/CalibrationCertificate.java
@@ -2,10 +2,12 @@ package de.ptb.common.dcc.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Data
+@Document
 public class CalibrationCertificate {
 
   @Id
